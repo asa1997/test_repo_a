67d6e2fadb58b87a35f2cc3a9fd442894943f9b4
Arun Suresh <58501088+asa1997@users.noreply.github.com>
2023-04-13 16:04:38 +0530

    Update besman-BesDevKit-dev-env.sh

--- BesDevKit/0.0.1/besman-BesDevKit-dev-env.sh
+++ BesDevKit/0.0.1/besman-BesDevKit-dev-env.sh
@@ -6,6 +6,51 @@ function __besman_install_BesDevKit-dev-env
     __besman_install_python || return 1
     __besman_install_pip3 || return 1
     # __besman_install_code || return 1
+    
+    [[ ! -d $HOME/besecure-developer-toolkit ]] && git clone https://github.com/asa1997/besecure-developer-toolkit
+
+}
+
+function __besman_uninstall_BesDevKit-dev-env
+{
+    echo "UnInstalled"
+} 
+
+function __besman_install_python
+{
+    if [[ -n "$(which python3)" ]]; then
+        echo "Python : requirement satisfied" 
+        return 0
+    fi
+    echo "Installing python"
+    sudo apt update
+    sudo apt install software-properties-common
+    sudo add-apt-repository ppa:deadsnakes/ppa
+    sudo apt update
+    sudo apt install python3.8
+    python3 --version
+    if [[ "$?" -ne "0" ]]; then
+        echo "Python installation failed" 
+        return 1
+    fi
+}
+
+function __besman_install_pip3
+{
+    [[ -n $(which pip3) ]] && echo "Pip3: requirement satisified" && return 0
+    echo "Installing pip"
+    sudo apt install python3-pip
+    python3 -m pip --version
+    [[ "$?" -ne "0" ]] && echo "Pip installation failed" && return 1
+}
+#!/bin/bash
+
+function __besman_install_BesDevKit-dev-env
+{
+    
+    __besman_install_python || return 1
+    __besman_install_pip3 || return 1
+    # __besman_install_code || return 1
     git clone https://github.com/asa1997/besecure-developer-toolkit
 
 }
